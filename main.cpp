e9cf25e1889922d010a9621b3babfd4a811ac06f
ArvinoBoro <arvin.ziba@ontariotechu.net>
2024-02-15 21:24:54 -0500

    Basic Functionality for the Data Structure Operations
    Additions
    
    1. Print the contents of the 2D vector.
    
    2. Search for a product, given a search term and its content.
    
    3. Delete a product.
    
    4. Update a product's attributes.
    
    5. Add a new product and specify it's attributes.
    
    6. Function for converting a string to all lowercase.

--- main.cpp
+++ main.cpp
@@ -2,17 +2,32 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <cctype>
+#include <algorithm>
+#include <cmath>
 
 using namespace std;
 
-void insert_attribute();                                // Function prototypes
+void insert_product();                                // Function prototypes
 void update_attribute();
 void delete_attribute();
 void search_attribute();
 void sort_attributes();
 void display_products();
+void write_to_file();
 
 vector<vector<string>> matrix;                          // Declare a global 2D vector
+
+
+std::string toLowercase(const std::string& str) {
+    std::string result = str; // Make a copy of the original string
+
+    // Convert each character to lowercase
+    std::transform(result.begin(), result.end(), result.begin(),
+                   [](unsigned char c) { return std::tolower(c); });
+
+    return result;
+}
 
 int main()
 {
@@ -35,6 +50,7 @@ int main()
             if (line[i] == ',') {                       // Check for comma seperating each attribute
                 matrix[j].push_back(attribute);         // Push the complete attribute to the vector indexed by j
                 attribute.clear();
+                i++;
             }
             else {
                 attribute += line[i];                   // Concatenate the attribute with the character indexed by i
@@ -57,13 +73,15 @@ int main()
     cout << "(4) Search" << '\n';
     cout << "(5) Sort" << '\n';
     cout << "(6) Display Products" << '\n';
+    cout << "(7) Write to text file" << '\n';
     cout << "Please select an option: ";
 
     cin >> user_option;
 
     switch (user_option) {
         case 1:
-            insert_attribute();
+            insert_product();
+            display_products();
         case 2:
             update_attribute();
         case 3:
@@ -74,27 +92,120 @@ int main()
             sort_attributes();
         case 6:
             display_products();
+        case 7:
+            write_to_file();
 
     }
     return 0;
 }
 
-void insert_attribute() {
+void insert_product() {
 
+    int position;
+    vector<string> new_product(5);
 
+    cout << '\n' << "Select the position: ";
+    cin >> position;
+    cin.ignore();
+    cout << '\n';
+
+    cout << "ID: ";
+    getline(cin, new_product[0]);
+    cout << '\n';
+
+    cout << "Name: ";
+    getline(cin, new_product[1]);
+    cout << '\n';
+
+    cout << "Price: ";
+    getline(cin, new_product[2]);
+    cout << '\n';
+
+    cout << "Category: ";
+    getline(cin, new_product[3]);
+    cout << '\n';
+
+    matrix.insert(matrix.begin() + position - 1, new_product);
 }
 
 void update_attribute() {
 
+    int position, option;
+
+    cout << "Enter the position of the product: ";
+    cin >> position;
+
+    cout << "(1) ID\n";
+    cout << "(2) Name\n";
+    cout << "(3) Price\n";
+    cout << "(4) Category\n";
+
+    cout << "What would you like to edit?: ";
+    cin >> option;
+    cin.ignore();
+
+    std::cout << "Enter the new attribute: ";
+
+    getline(cin, matrix[position-1][option-1]);
 
 }
 
 void delete_attribute() {
 
+    int position;
+
+    cout << "Enter the position of the product: ";
+    cin >> position;
+
+    matrix.erase(matrix.begin() + position - 1);
+
 }
 
 
 void search_attribute() {
+
+    int search_term;
+    int result_count = 0;
+    string content;
+
+
+
+    cout << "(1) ID\n";
+    cout << "(2) Name\n";
+    cout << "(3) Price\n";
+    cout << "(4) Category\n";
+
+    cout << "Enter the search term: ";
+    cin >> search_term;
+    cout << '\n';
+
+    cout << "Enter its content: ";
+    cin >> content;
+    cout << '\n';
+
+    if (is_sorted) {
+
+        int mid = floor((matrix.size() + 1) / 2 )
+
+        if ()
+    }
+
+    } else {
+
+        for (int i = 0; i < matrix.size(); i++) {
+
+        if (toLowercase( matrix[i][search_term - 1] ) == toLowercase(content)  ) {
+
+            result_count++;
+            cout << "Search result " << result_count << ":\n";
+            cout << matrix[i][0] << " ";
+            cout << matrix[i][1] << " ";
+            cout << matrix[i][2] << " ";
+            cout << matrix[i][3] << "\n\n";
+
+        }
+
+    }
 
 }
 
@@ -104,6 +215,26 @@ void sort_attributes() {
 
 
 void display_products() {
+
+    for (int i = 0; i < matrix.size(); i++) {
+
+        cout << "(";
+
+        if (i < 9)
+            cout << "0";
+
+        cout << i+1 << ") ";
+
+        for (int j = 0; j < matrix[j].size(); j++) {
+            cout << matrix[i][j] << "  ";
+        }
+        cout << '\n';
+    }
+
+}
+
+
+void write_to_file() {
 
 
 }
