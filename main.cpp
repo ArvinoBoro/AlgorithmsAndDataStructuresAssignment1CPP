ea9beab932ec40ed82fd501cd4f8598518beab37
ArvinoBoro <arvin.ziba@ontariotechu.net>
2024-02-17 17:20:45 -0500

    Final Minor Changes and Fixes
    Additions & Changes:
    
    1. More comments
    
    2. Removed file write function
    
    3. Removed binary search function
    
    4. Fixed a few errors and unexpected behaviours.
    
    5. Added a few newline characters to cout.

--- main.cpp
+++ main.cpp
@@ -8,19 +8,17 @@
 
 using namespace std;
 
-void insert_product();                                // Function prototypes
+void insert_product();                                  // Function prototypes
 void update_attribute();
 void delete_product();
 void search_attribute();
 void sort_products();
 void display_products();
-void write_to_file();
 
 vector<vector<string>> matrix;                          // Declare a global 2D vector
-bool sorted = false;
 
 
-std::string toLowercase(const std::string& str) {
+std::string toLowercase(const std::string& str) {       // Function to convert a string to all lowercase, courtesy of ChatGPT.
     std::string result = str; // Make a copy of the original string
 
     // Convert each character to lowercase
@@ -35,7 +33,6 @@ int main()
     string line;                                        // String for storing a single line in the text file
     ifstream fin;                                       // Object for reading from and writing to files
     string attribute;                                   // String for temporarily storing an attribute
-
     int j = 0;
 
     fin.open("product_data.txt");                       // Open the text file
@@ -43,47 +40,46 @@ int main()
     while (getline(fin, line)) {    //
 
         vector<string> hello = {};                      // Initialize an empty string vector
-
         matrix.push_back(hello);                        // Push the empty string vector to the matrix
 
         for (int i = 0; i < line.length(); i++) {
 
-            if (line[i] == ',') {                       // Check for comma seperating each attribute
+            if (line[i] == ',') {
+                                                        // Check for comma seperating each attribute
                 matrix[j].push_back(attribute);         // Push the complete attribute to the vector indexed by j
                 attribute.clear();
                 i++;
-            }
-            else {
+
+            } else {
+
                 attribute += line[i];                   // Concatenate the attribute with the character indexed by i
             }
         }
 
         matrix[j].push_back(attribute);                 // Push the last complete attribute to the vector
         attribute.clear();
-
         j++;
     }
 
     fin.close();                                        // Close the currently open text file
 
-
     while (true) {
 
         int user_option;
 
+        cout << "Menu\n";
         cout << "(1) Insert" << '\n';
         cout << "(2) Update" << '\n';
         cout << "(3) Delete" << '\n';
         cout << "(4) Search" << '\n';
-        cout << "(5) Sort" << '\n';
+        cout << "(5) Bubble Sort" << '\n';
         cout << "(6) Display Products" << '\n';
-        cout << "(7) Write to text file" << '\n';
-        cout << "(8) Exit" << '\n';
+        cout << "(7) Exit" << '\n';
         cout << "Please select an option: ";
-
         cin >> user_option;
+        cout << '\n';
 
-        switch (user_option) {
+        switch (user_option) {                          // Call to each function managed by the switch
             case 1:
                 insert_product();
                 break;
@@ -103,9 +99,6 @@ int main()
                 display_products();
                 break;
             case 7:
-                write_to_file();
-                break;
-            case 8:
                 return 0;
             default:
                 cout << "Invalid option. Please try again. \n";
@@ -117,30 +110,26 @@ int main()
 void insert_product() {
 
     int position;
-    vector<string> new_product(5);
+    vector<string> new_product(5);                      // Declare new vector for new product attributes
 
     cout << '\n' << "Select the position: ";
     cin >> position;
     cin.ignore();
-    cout << '\n';
 
     cout << "ID: ";
     getline(cin, new_product[0]);
-    cout << '\n';
 
     cout << "Name: ";
     getline(cin, new_product[1]);
-    cout << '\n';
 
     cout << "Price: ";
     getline(cin, new_product[2]);
-    cout << '\n';
 
     cout << "Category: ";
     getline(cin, new_product[3]);
     cout << '\n';
 
-    matrix.insert(matrix.begin() + position - 1, new_product);
+    matrix.insert(matrix.begin() + position - 1, new_product);      // Inserts filled vector before the element at the specified position in matrix
 }
 
 void update_attribute() {
@@ -148,21 +137,20 @@ void update_attribute() {
     int position, option;
 
     cout << "Enter the position of the product: ";
-    cin >> position;
+    cin >> position;                                                // Gets the position of the product as displayed using display_products()
 
     cout << "(1) ID\n";
     cout << "(2) Name\n";
     cout << "(3) Price\n";
     cout << "(4) Category\n";
-
     cout << "What would you like to edit?: ";
     cin >> option;
-    cin.ignore();
+    cin.ignore();                                                   // Clears the input buffer from the newline character \n
 
     std::cout << "Enter the new attribute: ";
-
     getline(cin, matrix[position-1][option-1]);
 
+    cout << '\n';
 }
 
 void delete_product() {
@@ -173,6 +161,8 @@ void delete_product() {
     cin >> position;
 
     matrix.erase(matrix.begin() + position - 1);
+
+    cout << '\n';
 }
 
 
@@ -180,7 +170,6 @@ void search_attribute() {
 
     int search_term;
     int result_count = 0;
-    vector<int> matched_product_indices;
     string content;
 
     cout << "(1) ID\n";
@@ -189,93 +178,44 @@ void search_attribute() {
     cout << "(4) Category\n";
 
     cout << "Enter the search term: ";
-    cin >> search_term;
+    cin >> search_term;                                 // Asks the user if they're search with ID (1), name (2), price (3), or cateogry (4)
     cout << '\n';
 
     cout << "Enter its content: ";
     cin >> content;
     cout << '\n';
 
-    if (sorted && (search_term == 3 )) {
+    for (int i = 0; i < matrix.size(); i++) {
 
-        int i = 1;
-        int j = matrix.size();
-        int k = 0;
-        float price = stof(content);
+        if (toLowercase( matrix[i][search_term - 1] ) == toLowercase(content)  ) {// Compares the search content to the queried content in their lowercase form
 
-        while (i < j) {
-
-            int mid = floor((i + j) / 2 );
-
-            if (price > stof(matrix[mid-1][2]))
-                i = mid + 1;
-
-            else
-                j = mid;
+            result_count++;
+            cout << matrix[i][0] << " ";
+            cout << matrix[i][1] << " ";
+            cout << matrix[i][2] << " ";
+            cout << matrix[i][3] << "\n\n";
         }
-
-        if (price == stof(matrix[i-1][2])) {
-
-            for (int k = 0; price == stoi(matrix[i + k][2]); k++)
-
-                matched_product_indices.push_back(i+k);
-
-
-            for (int l = -2; price == stof(matrix[i + l][2]); l--)
-
-                matched_product_indices.push_back(i+l);
-
-
-            for (int m = 0; m < matched_product_indices.size(); m++) {
-
-                cout << matrix[matched_product_indices[m]][0] << " ";
-                cout << matrix[matched_product_indices[m]][1] << " ";
-                cout << matrix[matched_product_indices[m]][2] << " ";
-                cout << matrix[matched_product_indices[m]][3] << "\n\n";
-            }
-
-        } else {
-
-            cout << "No results found.\n\n";
-        }
-
-    } else {
-
-        for (int i = 0; i < matrix.size(); i++) {
-
-            if (toLowercase( matrix[i][search_term - 1] ) == toLowercase(content)  ) {
-
-                result_count++;
-                cout << matrix[i][0] << " ";
-                cout << matrix[i][1] << " ";
-                cout << matrix[i][2] << " ";
-                cout << matrix[i][3] << "\n\n";
-
-            }
-        }
-
-        if (result_count == 0)
-            cout << "No results found.\n\n";
     }
 
+    if (result_count == 0)
+        cout << "No results found.\n\n";
 }
 
 
-void sort_products() {
+void sort_products() {              // Bubble sort algorithm
 
-    for (int i = 0; i < matrix.size()-1; i++) {
-        for (int j = 0; j < matrix.size()-i-1; j++) {
+    for (int i = 0; i < matrix.size()-1; i++) {                     // Iterates over every element in the matrix except the last
 
-            if (stoi(matrix[j][2]) > stoi(matrix[j+1][2])) {
+        for (int j = 0; j < matrix.size()-i-1; j++) {               // Decreases the iteration range by i-1 because the largest largest unsorted element gets bubbled to the end
+
+            if (stof(matrix[j][2]) > stof(matrix[j+1][2])) {       // Checks if the price of the vector indexed by j is greater than the vector indexed by j+1
 
                 vector<string> temp = matrix[j];
                 matrix[j] = matrix[j+1];
-                matrix[j + 1] = temp;
+                matrix[j + 1] = temp;                               // Swaps the vector indexed by j and the vector indexed by j+1
             }
         }
     }
-
-    sorted = true;
 }
 
 
@@ -285,21 +225,18 @@ void display_products() {
 
         cout << "(";
 
-        if (i < 9)
+        if (i < 9)                                      // Prepends single digit 1-based indices with a 0
+
             cout << "0";
 
         cout << i+1 << ") ";
 
         for (int j = 0; j < matrix[j].size(); j++) {
-            cout << matrix[i][j] << "  ";
+
+            cout << matrix[i][j] << "  ";               // Adds a space between each attribute in the same product
         }
-        cout << "\n";
+        cout << "\n\n";
     }
 
 }
 
-
-void write_to_file() {
-
-
-}
